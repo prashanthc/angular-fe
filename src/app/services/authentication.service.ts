d1202fca693e937c0079a162474b68cf4344e514
prashanthc <chaitankarp@gmail.com>
2019-09-06 16:28:15 +0530

    New Service

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  private currentUserSubject: BehaviorSubject<User>; 
+  public currentUser: Observable<User>;
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable()
+  }
+
+  public get getCurrentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string) {
+    return this.http.post('session', {username, password})
+      .pipe(map(response=>{
+        let user: any = response;
+        if (user && user.token) {
+          localStorage.setItem('currentUser', JSON.stringify(user));
+        }
+      }))
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+}
